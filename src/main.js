4bf8e9434e2e08207be44841ae006a6a6d50f272
Thijs van Beek <thijs@midasweb.nl>
2018-08-16 15:33:00 +0200

    reorderd import to remove linting errors

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+
 import App from './App';
 import router from './router';
-import BootstrapVue from 'bootstrap-vue'
-
-require('jquery');
-require('bootstrap/dist/js/bootstrap.js');
 
 import '@/scss/bootpresso.scss';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+require('jquery');
+require('bootstrap/dist/js/bootstrap.js');
 
 Vue.use(BootstrapVue);
 
